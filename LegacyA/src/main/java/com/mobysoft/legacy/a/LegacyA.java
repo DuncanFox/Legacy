4fa7218110813a1aa823a9310ff2aee356bb958c
Duncan <duncan.sanders-fox@mobysoft.com>
2016-03-17 15:27:31 +0000

    Initial Checkin 2

---
+++ LegacyA/src/main/java/com/mobysoft/legacy/a/LegacyA.java
@@ -0,0 +1,7 @@
+package com.mobysoft.legacy.a;
+
+/**
+ * Created by Duncan on 17/03/2016.
+ */
+public class LegacyA {
+}
---
+++ LegacyB/src/main/java/com/mobysoft/legacy/b/LegacyB.java
@@ -0,0 +1,7 @@
+package com.mobysoft.legacy.b;
+
+/**
+ * Created by Duncan on 17/03/2016.
+ */
+public class LegacyB {
+}
